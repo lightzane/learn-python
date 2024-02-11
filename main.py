fd9816e43fa4b4e0ff7ca0af83917b8174c73e06
lightzane <jp.eaguilar@gmail.com>
2024-02-11 19:25:56 +0800

    initial

---
+++ main.py
@@ -0,0 +1,26 @@
+# Python Object-Oriented Programming
+
+class Employee:
+    # constructor()
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = f'{first}.{last}@company.com'.lower()
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+# emp_1 and emp_2 are instances of Employee
+emp_1 = Employee('John', 'Doe', 50)
+emp_2 = Employee('Ji-Eun', 'Lee', 100)
+
+print(emp_1.email) # => john.doe@company.com
+print(emp_2.email) # => ji-eun.lee@company.com
+
+print(emp_1.fullname()) # => John Doe
+print(emp_2.fullname()) # => Ji-Eun Lee
+
+print(Employee.fullname(emp_1)) # => John Doe
+print(Employee.fullname(emp_2)) # => Ji-Eun Lee
+    
